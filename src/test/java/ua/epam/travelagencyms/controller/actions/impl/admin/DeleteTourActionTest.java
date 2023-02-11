69b949b899876adc76304c9d36b088bd4ef8e8ce
Nadiia Rubantseva <nadyafranko@gmail.com>
2023-02-11 11:37:56 +0200

    1. add deleteTourAction unit test.

---
+++ src/test/java/ua/epam/travelagencyms/controller/actions/impl/admin/DeleteTourActionTest.java
@@ -0,0 +1,41 @@
+package ua.epam.travelagencyms.controller.actions.impl.admin;
+
+import org.junit.jupiter.api.Test;
+import ua.epam.travelagencyms.controller.actions.util.MyRequest;
+import ua.epam.travelagencyms.controller.context.AppContext;
+import ua.epam.travelagencyms.exceptions.ServiceException;
+import ua.epam.travelagencyms.model.services.TourService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static ua.epam.travelagencyms.ConstantsForTest.ONE;
+import static ua.epam.travelagencyms.ConstantsForTest.POST;
+import static ua.epam.travelagencyms.controller.actions.ActionUtil.getActionToRedirect;
+import static ua.epam.travelagencyms.controller.actions.constants.ActionNames.VIEW_TOURS_ACTION;
+import static ua.epam.travelagencyms.controller.actions.constants.ParameterValues.SUCCEED_DELETE;
+import static ua.epam.travelagencyms.controller.actions.constants.Parameters.MESSAGE;
+import static ua.epam.travelagencyms.controller.actions.constants.Parameters.TOUR_ID;
+
+class DeleteTourActionTest {
+
+    private final HttpServletRequest request = mock(HttpServletRequest.class);
+    private final HttpServletResponse response = mock(HttpServletResponse.class);
+    private final AppContext appContext = mock(AppContext.class);
+    private final TourService tourService = mock(TourService.class);
+
+    @Test
+    void testExecutePost() throws ServiceException {
+        MyRequest myRequest = new MyRequest(request);
+        when(request.getMethod()).thenReturn(POST);
+        when(request.getParameter(TOUR_ID)).thenReturn(ONE);
+        when(appContext.getTourService()).thenReturn(tourService);
+        doNothing().when(tourService).delete(ONE);
+        String path = new DeleteTourAction(appContext).execute(myRequest, response);
+
+        assertEquals(getActionToRedirect(VIEW_TOURS_ACTION), path);
+        assertEquals(SUCCEED_DELETE, myRequest.getSession().getAttribute(MESSAGE));
+    }
+}
